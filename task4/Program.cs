d70c3077e56af1cd0e732fb9c1f5a3d963392bad
Alexsander <zubanov.a.a@inbox.ru>
2023-06-20 12:59:23 +0400

    init commit

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int check = 1;
+while (number >= check)
+{
+    if (check % 2 == 0)
+        System.Console.Write($" {check},");
+    check++;
+}
+
+
